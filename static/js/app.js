4c83c83e8308c7b34a43ba8f3abb8431e80057b6
Nwabueze Christian Izuchukwu <izuchukwujnwabueze@gmail.com>
2025-04-10 19:17:14 +0100

    Delete static directory

--- static/js/app.js
+++
@@ -1,9 +0,0 @@
-function sendMessage() {
-    var msg = document.getElementById("message").value;
-    if (msg.trim() === "") return;
-  
-    var chatBox = document.getElementById("chat-box");
-    chatBox.innerHTML += "<p><b>You:</b> " + msg + "</p>";
-  
-    document.getElementById("message").value = "";
-}
