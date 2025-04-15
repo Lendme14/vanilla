f3653ee025e1225ad68b08230f5ab85e61e8f262
Nwabueze Christian Izuchukwu <izuchukwujnwabueze@gmail.com>
2025-04-15 05:45:43 -0700

    Update firebase-config.js

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -6,12 +6,12 @@ import { getDatabase } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-
 import { getStorage } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: "AIzaSyCstSPisweA_Wc9rO0MFFa4k8mH7ddnuLY",
+  authDomain: "zuchx-authentication.firebaseapp.com",
+  projectId: "zuchx-authentication",
+  storageBucket: "zuchx-authentication.firebasestorage.app",
+  messagingSenderId: "1060255681788",
+  appId: "1:1060255681788:web:0db6833f84ad3566545b79"
 };
 
 const app = initializeApp(firebaseConfig);
